c8298a8bdcff2c2e4b399b1e2615580ecb457d2a
40423218 <42423218@gm.nfu.edu.tw>
2016-01-14 20:49:26 +0800

    40423209

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -7,8 +7,8 @@ from __future__ import unicode_literals
 
 import os
 
-AUTHOR = '40423212'
-SITENAME = '2015FALL 40423212 CPB 作業'
+AUTHOR = '40423209'
+SITENAME = '2015FALL 40423209 CPB 作業'
 SITEURL = ''
 
 PATH = 'content'
@@ -24,14 +24,16 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),
-         ('倉儲','http://40423212.github.io/cpbtest2/'))
-
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
           ('github', 'http://github.com/ametaireau'),)
+          
+# Blogroll
+LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
+         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('40423209-gh-pages','http://40423209.github.io/cpbtest2/'))
+
+
+
           
 DEFAULT_PAGINATION = 10
 
